6e31f7be8ec720a97c7b5e4be13f1644737db4db
KimNikita <ghj710@mail.ru>
2020-02-17 12:30:19 +0300

    Done task 1

---
+++ Task_1/MainProject.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <math.h>
+#include <string>
+
+#define long long int int
+using namespace std;
+
+signed main()
+{
+	int n, sum = 0, isum = 0, msum = -1;
+	cin >> n;
+	string s[100];
+	int p, m, a, b, c, d, e;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> s[i] >> p >> m >> a >> b >> c >> d >> e;
+		sum = p * 100 - m * 50 + a + b + c + d + e;
+		if (sum > msum)
+		{
+			isum = i;
+			msum = sum;
+		}
+	}
+	cout << s[isum];
+	return 0;
+}
